a9469509da6afcdc488658c5981eea9f12608f1a
Chamaru Amasara <mailtochamaru@gmail.com>
2022-11-29 09:27:41 +0530

    Added demo assets and stuff

---
+++ project/layout/_default.php
@@ -0,0 +1,25 @@
+<!--begin::App-->
+<div class="d-flex flex-column flex-root app-root" id="kt_app_root">
+	<!--begin::Page-->
+	<div class="app-page flex-column flex-column-fluid" id="kt_app_page">
+		<?php include 'layout/partials/_header.php' ?>
+		<!--begin::Wrapper-->
+		<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+			<?php include 'layout/partials/_sidebar.php' ?>
+			<!--begin::Main-->
+			<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+				<!--begin::Content wrapper-->
+				<div class="d-flex flex-column flex-column-fluid">
+					<?php include 'layout/partials/_content.php' ?>
+				</div>
+				<!--end::Content wrapper-->
+				<?php include 'layout/partials/_footer.php' ?>
+			</div>
+			<!--end:::Main-->
+		</div>
+		<!--end::Wrapper-->
+	</div>
+	<!--end::Page-->
+</div>
+<!--end::App-->
+<?php include 'partials/_drawers.php' ?>
