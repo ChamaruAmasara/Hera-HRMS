b52ac0a1850e44ea141b39bc046e6b588f1b2818
Chamaru Amasara <mailtochamaru@gmail.com>
2023-01-03 12:29:45 +0530

    database connection.

---
+++ project/db-config.php
@@ -0,0 +1,21 @@
+<?php 
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "hera";
+$port = 3306;
+
+
+// Create connection
+try{
+    $connection = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+
+    $connection -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOEXception $e){
+    echo "Error in connection ".$e->getMessage();
+
+}
+?>
