a4772cf86a246714582cce3e4e98fa94e547158f
Chamaru Amasara <mailtochamaru@gmail.com>
2023-01-05 02:12:34 +0530

    updated employee details page

--- project/pages/employee/details.php
+++ project/pages/employee/details.php
@@ -1,50 +1,18 @@
 <?php
 	include_once PROJECT_ROOT_PATH.'/includes/userdetails.inc.php';
+	include_once PROJECT_ROOT_PATH.'/includes/dbconfig.inc.php';
+	$connection=openDatabaseConnection();
 	$empCountSQL="SELECT COUNT(EmployeeID) AS empCount FROM employee";
 	$empCountResult=mysqli_query($connection,$empCountSQL);
 	$empCountRow=mysqli_fetch_assoc($empCountResult);
 	$empCount=htmlspecialchars($empCountRow['empCount']);
 ?>
 
-<html lang="en">
-	<!--begin::Head-->
-	<head><base href="../../"/>
-		
-		<meta charset="utf-8" />
-		<meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Flask & Laravel versions. Grab your copy now and get life-time updates for free." />
-		<meta name="keywords" content="metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Flask & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:locale" content="en_US" />
-		<meta property="og:type" content="article" />
-		<meta property="og:title" content="Metronic - Bootstrap 5 HTML, VueJS, React, Angular. Laravel, Asp.Net Core, Ruby on Rails, Spring Boot, Blazor, Django, Express Node.js & Flask Admin Dashboard Theme" />
-		<meta property="og:url" content="https://keenthemes.com/metronic" />
-		<meta property="og:site_name" content="Keenthemes | Metronic" />
-		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-		<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-		<!--begin::Fonts(mandatory for all pages)-->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-		<!--end::Fonts-->
-		<!--begin::Vendor Stylesheets(used for this page only)-->
-		<link href="assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
-		<!--end::Vendor Stylesheets-->
-		<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
-		<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-		<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-		<!--end::Global Stylesheets Bundle-->
-	</head>
-	<!--end::Head-->
-	<!--begin::Body-->
-	<body id="kt_app_body" data-kt-app-layout="dark-sidebar" data-kt-app-header-fixed="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
-		<!--begin::App-->
-		<div class="d-flex flex-column flex-root app-root" id="kt_app_root">
-				<!--begin::Main-->
-				<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
-					<!--begin::Content wrapper-->
-					<div class="d-flex flex-column flex-column-fluid">
+
 						<!--begin::Content-->
 						<div id="kt_app_content" class="app-content flex-column-fluid">
 							<!--begin::Content container-->
-							<div id="kt_app_content_container" class="app-container container-xxl">
+							<div id="kt_app_content_container" class="app-container container-fluid">
 								<!--begin::Search vertical-->
 								<div class="d-flex flex-column flex-lg-row">
 									<!--begin::Aside-->
@@ -524,14 +492,6 @@
 							<!--end::Content container-->
 						</div>
 						<!--end::Content-->
-					</div>
-					<!--end::Content wrapper-->
-				</div>
-				<!--end:::Main-->
-		</div>
-		<!--end::App-->
-		
-
 		<!--begin::Scrolltop-->
 		<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
 			<!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
